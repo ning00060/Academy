3492ec6a1ef313c094a07e1e7269e6cbb2dc2d9d
lgw <spglados@gmail.com>
2024-07-19 17:46:07 +0900

    logiun susses

--- src/main/java/com/tenco/Repo/user/UserRepositoryImpl.java
+++ src/main/java/com/tenco/Repo/user/UserRepositoryImpl.java
@@ -11,13 +11,13 @@ import com.tenco.util.DBUtil;
 public class UserRepositoryImpl implements UserRepository{
 
 	@Override
-	public UserDTO userLogin(String id, String password) {
+	public UserDTO userLogin(int id, String password) {
 		//TODO - sql 작성
 		String sql = " SELECT id,permission_level FROM tb_user where id = ? and password = ? ";
 		UserDTO verifiedUser = null;
 		try (Connection conn = DBUtil.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql)){
-			pstmt.setString(1, id);
+			pstmt.setInt(1, id);
 			pstmt.setString(2, password);
 			ResultSet rs = pstmt.executeQuery();
 			if (rs.next()) {
@@ -29,7 +29,7 @@ public class UserRepositoryImpl implements UserRepository{
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		System.out.println(verifiedUser.toString());
 		return verifiedUser;
 	}
 
--- src/main/java/com/tenco/model/user/UserDTO.java
+++ src/main/java/com/tenco/model/user/UserDTO.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 
 @AllArgsConstructor
 @Getter
 @Builder
+@ToString
 
 public class UserDTO {
 	
