d4072beaa39ff1b31bf0777b8646e7e204a6d48d
wjdgns5 <rhfovkeh5@naver.com>
2024-07-22 12:41:33 +0900

    22일

---
+++ src/main/java/com/tenco/model/temp/NoticeDTO.java
@@ -0,0 +1,14 @@
+package com.tenco.model.temp;
+
+import java.sql.Timestamp;
+
+public class NoticeDTO {
+
+	private int id;
+	private String category;
+	private String title;
+	private String content;
+	private Timestamp createdTime;
+	private int views;
+	
+}
---
+++ src/main/java/com/tenco/model/temp/NoticeFileDTO.java
@@ -0,0 +1,11 @@
+package com.tenco.model.temp;
+
+public class NoticeFileDTO {
+
+	private int noticeId;
+	private String originFilename;
+	private String uuidFilename;
+	
+	
+	
+}
