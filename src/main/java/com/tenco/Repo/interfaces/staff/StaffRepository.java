96e39038ee95406d03f13eacc13a40d256942403
ning00060 <ning00060@gmail.com>
2024-07-26 11:49:24 +0900

    1

--- src/main/java/com/tenco/Repo/interfaces/staff/StaffRepository.java
+++ src/main/java/com/tenco/Repo/interfaces/staff/StaffRepository.java
@@ -2,9 +2,23 @@ package com.tenco.Repo.interfaces.staff;
 
 import com.tenco.model.staff.StaffDTO;
 
+import java.util.List;
+
+import com.tenco.model.staff.DepartmentDTO;
+
 public interface StaffRepository {
 
 	StaffDTO selectUserIdByNameEmail();
 	StaffDTO selectUserIdByNameIdEmail();
 	StaffDTO getAllInfoById(int id);
+	
+	//학과 관련
+	void addDepartment(DepartmentDTO departmentDTO);
+	void deleteDepartment(int id);
+	List<DepartmentDTO> getAllDepartments();
+	DepartmentDTO getDepartment(int id);
+	void upDateDepartmentById(int id);
+	
+	
+	
 }
