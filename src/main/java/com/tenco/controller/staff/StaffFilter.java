53119ea877996c0a421dba0b6ae89dbda8e8347d
jinnymo <ehddbs1105@gmail.com>
2024-07-29 16:27:04 +0900

    session check

--- src/main/java/com/tenco/controller/staff/StaffFilter.java
+++ src/main/java/com/tenco/controller/staff/StaffFilter.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebFilter("/professor/*")
+@WebFilter("/staff/*")
 public class StaffFilter extends HttpFilter implements Filter {
 
 	public StaffFilter() {
